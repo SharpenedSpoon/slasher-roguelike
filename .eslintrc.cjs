bc31227b645206283d1b84edd8ea339d368ec7f4
Ian Fox <ian@statuscontrols.com>
2022-04-12 21:56:03 -0700

    minor changes.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,29 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
+	"root": true,
+	"extends": [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/eslint-config-typescript/recommended",
+		'plugin:vue/vue3-recommended'
+	],
+	"env": {
+		"vue/setup-compiler-macros": true
+	},
+	rules: {
+		"vue/html-indent": ["error", 'tab'],
+		
+		"vue/component-tags-order": ["error", {
+			"order": ["template", "script", "style"]
+		}],
+		
+		"vue/html-closing-bracket-spacing": ["error", {
+			"startTag": "never",
+			"endTag": "never",
+			"selfClosingTag": "always"
+		}],
+		
+		'vue/singleline-html-element-content-newline': 0
+	},
 }
